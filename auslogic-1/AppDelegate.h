58b5bfee19fac04729bd99af324b32054cc0d28f
Sergiy Kostrykin <sergiykostrykin@Dmitrys-Mac-mini.local>
2015-08-04 17:59:41 +0300

    Add project

---
+++ auslogic-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  auslogic-1
+//
+//  Created by Sergiy Kostrykin on 8/4/15.
+//  Copyright (c) 2015 Sergiy Kostrykin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ auslogic-1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  auslogic-1
+//
+//  Created by Sergiy Kostrykin on 8/4/15.
+//  Copyright (c) 2015 Sergiy Kostrykin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
